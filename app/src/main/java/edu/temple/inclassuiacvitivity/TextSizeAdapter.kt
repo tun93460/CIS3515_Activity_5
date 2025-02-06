8beb15d58b7ce1955c5ca338ebeff9dd984c2db4
tun93460 <ldgraham2002@gmail.com>
2025-02-06 11:11:42 -0500

    spinner now displays textSize options with respective textSize in the dropdown menu but at a consistent size when selected

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,42 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _sizes: Array<Int>) : BaseAdapter() {
+
+    private val context = _context
+    private val sizes = _sizes
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return sizes.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return sizes[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = sizes[position].toString()
+        textView.textSize = 22F
+        textView.setPadding(5,10,0,10)
+        return textView
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val textView = TextView(context)
+        textView.text = sizes[position].toString()
+        textView.textSize = sizes[position].toFloat()
+        textView.setPadding(5,10,0,10)
+        return textView
     }
 
 }
